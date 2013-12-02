b7a8fb2d08b90d0a4ef7ad8aede1c0d34241444c
Alexander Kit <source.kit@gmail.com>
2013-12-02 16:02:04 +0100

    [dist] traceur middleware

--- src/middleware/traceur.js
+++ src/middleware/traceur.js
@@ -1,8 +1,8 @@
 io
     .File
-    .middleware['coffee'] = function(file){
+    .middleware['traceur'] = function(file){
         
-        var Traceut = require('traceur');
+        var Traceur = require('traceur');
     
         if (typeof file.content !== 'string'){
             file.content = file.content.toString();
