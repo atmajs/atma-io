9abba7f45598877eb3cece8d5d3616c802419f6e
Alex Kit <source.kit@gmail.com>
2014-02-16 18:13:33 +0100

    [dist] io.glob.readFiles

--- src/exports.utils.js
+++ src/exports.utils.js
@@ -1,5 +1,12 @@
 io.glob = {
-    matchPath: glob_matchPath
+    matchPath: glob_matchPath,
+    readFiles: function(path){
+        
+        var strict = glob_getStrictPath(path),
+            rel = glob_getRelativePath(path);
+            
+        return new io.Directory(strict).readFiles(rel).files;
+    }
 };
 
 io.settings = function(settings){
