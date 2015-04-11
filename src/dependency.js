722b86688792c514a205ba7ebc4acb26287918a1
Alex Kit <source.kit@gmail.com>
2015-04-11 15:17:27 +0200

    fix (dependency loader) atma-logger

--- src/dependency.js
+++ src/dependency.js
@@ -7,14 +7,11 @@ var atma = typeof atma !== 'undefined'
 if (atma.Class == null) 
     atma = require('atma-libs/exports');
 
-if (global.logger == null) 
-    require('atma-logger');
-
 var __fs = require('fs'),
     _Array_slice = Array.prototype.slice,
     
     net = atma.net,
     Class = atma.Class,
-    logger = global.logger
+    logger = global.logger || require('atma-logger')
     ;
     
