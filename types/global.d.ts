f4abb851e469c9c2406057a269975c310db4d90e
Alex Kit <source.kit@gmail.com>
2017-07-20 18:01:46 +0200

    fix (types) File::uri

--- types/global.d.ts
+++ types/global.d.ts
@@ -17,27 +17,29 @@ declare namespace io {
     export class File {
         constructor (path: string | Uri)
         content: IContent
+        uri: Uri
+        sourceMap?: string
 
         read (options?: IReadOptions): IContent
-        readAsync (options?: IReadOptions): Promise<IContent>
+        readAsync (options?: IReadOptions): Deferred<IContent>
 
         write (content: IContent, options?: IReadOptions) : this
-        writeAsync (content: IContent, options?: IReadOptions): Promise<void>
+        writeAsync (content: IContent, options?: IReadOptions): Deferred<void>
 
         copyTo (targetPath: string): this
-        copyToAsync (targetPath: string): Promise<void>
+        copyToAsync (targetPath: string): Deferred<void>
 
         exists () : boolean
-        existsAsync () : Promise<boolean>
+        existsAsync () : Deferred<boolean>
 
         rename (fileName: string) : boolean
-        renameAsync (fileName: string) : Promise<boolean>
+        renameAsync (fileName: string) : Deferred<boolean>
 
         remove () : boolean
-        removeAsync () : Promise<boolean>
+        removeAsync () : Deferred<boolean>
 
         replace (match: string | RegExp | Function, withValue?: string, options?: IReadOptions): IContent
-        replaceAsync (match: string | RegExp | Function, withValue?: string, options?: IReadOptions): Promise<IContent>
+        replaceAsync (match: string | RegExp | Function, withValue?: string, options?: IReadOptions): Deferred<IContent>
 
         watch (onChange: (path: string ) => void)
         unwatch (onChange: Function)
@@ -54,22 +56,22 @@ declare namespace io {
         static processHooks (method: 'read' | 'write', file: File, config: any, onComplete: (file: File) => void): void
 
         static read (path: string | Uri, options?: IReadOptions): IContent
-        static readAsync (path: string | Uri,options?: IReadOptions): Promise<IContent>
+        static readAsync (path: string | Uri,options?: IReadOptions): Deferred<IContent>
 
         static write (path: string | Uri,content: IContent, options?: IReadOptions) : File
-        static writeAsync (path: string | Uri,content: IContent, options?: IReadOptions): Promise<void>
+        static writeAsync (path: string | Uri,content: IContent, options?: IReadOptions): Deferred<void>
 
         static copyTo (path: string | Uri, targetPath: string): File
-        static copyToAsync (path: string | Uri, targetPath: string): Promise<void>
+        static copyToAsync (path: string | Uri, targetPath: string): Deferred<void>
 
         static exists (path: string | Uri) : boolean
-        static existsAsync (path: string | Uri) : Promise<boolean>
+        static existsAsync (path: string | Uri) : Deferred<boolean>
 
         static rename (path: string | Uri, fileName: string) : boolean
-        static renameAsync (path: string | Uri, fileName: string) : Promise<boolean>
+        static renameAsync (path: string | Uri, fileName: string) : Deferred<boolean>
 
         static remove (path: string | Uri,) : boolean
-        static removeAsync (path: string | Uri,) : Promise<boolean>
+        static removeAsync (path: string | Uri,) : Deferred<boolean>
 
         /** Registers additional middlewares for the extension(s) */
         static registerExtensions (definition: {
@@ -83,7 +85,7 @@ declare namespace io {
         })
     }
 
-     export interface IFactory {
+        export interface IFactory {
         handlers: IVirtualFile[]
 
         registerHandler (regexp: RegExp, handler: IVirtualFile)
@@ -94,7 +96,7 @@ declare namespace io {
         [P in keyof T]?: T[P];
     }
     type IVirtualFile = Partial<File>
-    
+
     export interface IHookCollection {
         register (data: {
             regexp: RegExp
@@ -109,30 +111,30 @@ declare namespace io {
         readAsync  (file: File, config: IReadOptions, cb: Function): void
         writeAsync (file: File, config: IReadOptions, cb: Function): void
     }
-    
+
 
     export class Directory {
         file: File[]
         /** Path must ends with slash */        
         constructor (path: string)
         exists () : boolean
-        existsAsync () : Promise<boolean>
+        existsAsync () : Deferred<boolean>
         ensure () : this
-        ensureAsync () : Promise<this>
+        ensureAsync () : Deferred<this>
         readFiles (globPattern: string, globPatternExclude?: string): this
-        readFilesAsync (globPattern: string, globPatternExclude?: string): Promise<this>
+        readFilesAsync (globPattern: string, globPatternExclude?: string): Deferred<this>
 
         read (globPattern: string, globPatternExclude?: string): File[]
-        readAsync (globPattern: string, globPatternExclude?: string): Promise<File[]>
+        readAsync (globPattern: string, globPatternExclude?: string): Deferred<File[]>
 
         copyTo(target: string, options?: { verbose: boolean }): boolean
-        copyToAsync(target: string, options?: { verbose: boolean }): Promise<boolean>
+        copyToAsync(target: string, options?: { verbose: boolean }): Deferred<boolean>
 
         getName (): string
         rename (newName: string): void
-        renameAsync (newName: string): Promise<void>
+        renameAsync (newName: string): Deferred<void>
         remove () : void
-        removeAsync () : Promise<void>
+        removeAsync () : Deferred<void>
 
         watch (callback: Function)
         unwatch (callback: Function)
@@ -144,9 +146,9 @@ declare namespace io {
         constructor (path)
 
         protocol: string		
-		value: string
-		path: string
-		file: string
+        value: string
+        path: string
+        file: string
         extension: string
         
         cdUp (): this
@@ -156,8 +158,8 @@ declare namespace io {
         toLocalDir (): string
         toPathAndQuery (): string
         /**
-		 * @return Current Uri Path{String} that is relative to @arg1 Uri
-		 */
+         * @return Current Uri Path{String} that is relative to @arg1 Uri
+         */
         toRelativeString (uri: Uri): string
         
         isRelative (): boolean
@@ -169,6 +171,24 @@ declare namespace io {
 
     export namespace glob {
         export function read (globPattern: string): File[]
-        export function readAsync (globPattern: string): Promise<File[]>
+        export function readAsync (globPattern: string): Deferred<File[]>
     }
+
+    export class Deferred<T> extends Promise<T> {
+        done (done: (result: T) => void | Deferred<any>): this
+        fail (fail: (error: any | Error) => void): this
+        reject(error: any | Error) 
+        resolve(result?: T): this
+        always (always: Function): this
+
+        defer (): this
+        isResolved (): boolean
+        isRejected (): boolean
+        isBusy (): boolean
+        resolveDelegate (): (result: T | any) => void | any
+        rejectDelegate (): (result: Error | any) => void | any
+
+        static run<T> (fn: (resolve: Function, reject?: Function) => void, ctx?: any): Deferred<T>        
+    }
+
 }
