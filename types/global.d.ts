f0d7a3399d250b0fe23bba3acbdfc912afc3dbd1
Alex Kit <source.kit@gmail.com>
2017-07-21 17:29:34 +0200

    clone (types) atma utils Deferred

--- types/global.d.ts
+++ types/global.d.ts
@@ -21,30 +21,30 @@ declare namespace io {
         sourceMap?: string
 
         read (options?: IReadOptions): IContent
-        readAsync (options?: IReadOptions): Deferred<IContent>
+        readAsync (options?: IReadOptions): Deferred | PromiseLike<IContent>
 
         write (content: IContent, options?: IReadOptions) : this
-        writeAsync (content: IContent, options?: IReadOptions): Deferred<void>
+        writeAsync (content: IContent, options?: IReadOptions): Deferred | PromiseLike<void>
 
         copyTo (targetPath: string): this
-        copyToAsync (targetPath: string): Deferred<void>
+        copyToAsync (targetPath: string): Deferred | PromiseLike<void>
 
         exists () : boolean
-        existsAsync () : Deferred<boolean>
+        existsAsync () : Deferred | PromiseLike<boolean>
 
         rename (fileName: string) : boolean
-        renameAsync (fileName: string) : Deferred<boolean>
+        renameAsync (fileName: string) : Deferred | PromiseLike<boolean>
 
         remove () : boolean
-        removeAsync () : Deferred<boolean>
+        removeAsync () : Deferred | PromiseLike<boolean>
 
         replace (match: string | RegExp | Function, withValue?: string, options?: IReadOptions): IContent
-        replaceAsync (match: string | RegExp | Function, withValue?: string, options?: IReadOptions): Deferred<IContent>
+        replaceAsync (match: string | RegExp | Function, withValue?: string, options?: IReadOptions): Deferred | PromiseLike<IContent>
 
         watch (onChange: (path: string ) => void)
         unwatch (onChange: Function)
 
-        stats (): {}
+        stats (): fs.Stats
 
         static clearCache (path?: string)
         static disableCache ()
@@ -57,22 +57,22 @@ declare namespace io {
         static processHooks (method: 'read' | 'write', file: File, config: any, onComplete: (file: File) => void): void
 
         static read (path: string | Uri, options?: IReadOptions): IContent
-        static readAsync (path: string | Uri,options?: IReadOptions): Deferred<IContent>
+        static readAsync (path: string | Uri,options?: IReadOptions): Deferred | PromiseLike<IContent>
 
         static write (path: string | Uri,content: IContent, options?: IReadOptions) : File
-        static writeAsync (path: string | Uri,content: IContent, options?: IReadOptions): Deferred<void>
+        static writeAsync (path: string | Uri,content: IContent, options?: IReadOptions): Deferred | PromiseLike<void>
 
         static copyTo (path: string | Uri, targetPath: string): File
-        static copyToAsync (path: string | Uri, targetPath: string): Deferred<void>
+        static copyToAsync (path: string | Uri, targetPath: string): Deferred | PromiseLike<void>
 
         static exists (path: string | Uri) : boolean
-        static existsAsync (path: string | Uri) : Deferred<boolean>
+        static existsAsync (path: string | Uri) : Deferred | PromiseLike<boolean>
 
         static rename (path: string | Uri, fileName: string) : boolean
-        static renameAsync (path: string | Uri, fileName: string) : Deferred<boolean>
+        static renameAsync (path: string | Uri, fileName: string) : Deferred | PromiseLike<boolean>
 
         static remove (path: string | Uri,) : boolean
-        static removeAsync (path: string | Uri,) : Deferred<boolean>
+        static removeAsync (path: string | Uri,) : Deferred | PromiseLike<boolean>
 
         /** Registers additional middlewares for the extension(s) */
         static registerExtensions (definition: {
@@ -119,23 +119,23 @@ declare namespace io {
         /** Path must ends with slash */        
         constructor (path: string)
         exists () : boolean
-        existsAsync () : Deferred<boolean>
+        existsAsync () : Deferred | PromiseLike<boolean>
         ensure () : this
-        ensureAsync () : Deferred<this>
+        ensureAsync () : Deferred | PromiseLike<this>
         readFiles (globPattern: string, globPatternExclude?: string): this
-        readFilesAsync (globPattern: string, globPatternExclude?: string): Deferred<this>
+        readFilesAsync (globPattern: string, globPatternExclude?: string): Deferred | PromiseLike<this>
 
         read (globPattern: string, globPatternExclude?: string): File[]
-        readAsync (globPattern: string, globPatternExclude?: string): Deferred<File[]>
+        readAsync (globPattern: string, globPatternExclude?: string): Deferred | PromiseLike<File[]>
 
         copyTo(target: string, options?: { verbose: boolean }): boolean
-        copyToAsync(target: string, options?: { verbose: boolean }): Deferred<boolean>
+        copyToAsync(target: string, options?: { verbose: boolean }): Deferred | PromiseLike<boolean>
 
         getName (): string
         rename (newName: string): void
-        renameAsync (newName: string): Deferred<void>
+        renameAsync (newName: string): Deferred | PromiseLike<void>
         remove () : void
-        removeAsync () : Deferred<void>
+        removeAsync () : Deferred | PromiseLike<void>
 
         watch (callback: Function)
         unwatch (callback: Function)
@@ -172,24 +172,30 @@ declare namespace io {
 
     export namespace glob {
         export function read (globPattern: string): File[]
-        export function readAsync (globPattern: string): Deferred<File[]>
+        export function readAsync (globPattern: string): Deferred | PromiseLike<File[]>
     }
 
-    export class Deferred<T> extends Promise<T> {
-        done (done: (result: T) => void | Deferred<any>): this
+
+
+    type DeferredLike = Deferred | PromiseLike<any>;  
+    export class Deferred {
+        then(onOk: (...args: any[]) => void | DeferredLike, onFail?: (...args: any[]) => void | DeferredLike)
+        done (done: (...args: any[]) => void | DeferredLike): this
         fail (fail: (error: any | Error) => void): this
-        reject(error: any | Error) 
-        resolve(result?: T): this
+        reject(error: any | Error) : this
+        resolve(...args: any[]): this
         always (always: Function): this
 
         defer (): this
         isResolved (): boolean
         isRejected (): boolean
         isBusy (): boolean
-        resolveDelegate (): (result: T | any) => void | any
+        resolveDelegate (): (result: any) => void | any
         rejectDelegate (): (result: Error | any) => void | any
 
-        static run<T> (fn: (resolve: Function, reject?: Function) => void, ctx?: any): Deferred<T>        
+        static run (fn: (resolve: Function, reject?: Function) => void | Deferred, ctx?: any): DeferredLike
+        static resolve (...args: any[]): DeferredLike
+        static reject (...args: any[]): DeferredLike
     }
 
 }
