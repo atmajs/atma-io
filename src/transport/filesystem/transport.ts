b5452ff1e2fda11171c4e051b479c12692d88a1c
Alex Kit <source.kit@gmail.com>
2019-02-28 17:08:44 +0100

    v1.2.20

--- src/transport/filesystem/transport.ts
+++
@@ -1,9 +0,0 @@
-
-import { FileFsTransport } from './fs_file'
-import { ITransport } from '../custom';
-import { DirectoryFsTransport } from './fs_dir';
-
-export const FsTransport: ITransport = {
-    File: FileFsTransport,
-    Directory: DirectoryFsTransport
-}
